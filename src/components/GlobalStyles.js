290ef399372469b51fa11a57e11fc79c3c69476f
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-14 20:58:29 +0100

    navigation and search input setup

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -48,7 +48,7 @@ const GlobalStyles = createGlobalStyle`
     }
     input{
         font-weight: bold;
-    font-family: "Montserrat", sans-serif;
+      font-family: "Montserrat", sans-serif;
     }
 `;
 
