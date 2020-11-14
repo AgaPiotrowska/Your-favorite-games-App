ec9534f23fab4ab2531e764962406bb909a1177c
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-14 20:07:44 +0100

    stars rating added

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -9,6 +9,8 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({pathId}) => {
 
@@ -39,6 +41,19 @@ const GameDetail = ({pathId}) => {
         }
     };
 
+    const getStars = () => {
+        const stars=[];
+        const rating=Math.floor(game.rating);
+        for(let i=1; i<=5; i++){
+            if(i <= rating){
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            } else {
+                stars.push(<img alt="star" key={i} src={starEmpty}></img>)
+            }
+        }
+        return stars;
+    };
+
     const {screen, game, isLoading} = useSelector((state) => state.detail);
 
     return (
@@ -52,6 +67,7 @@ const GameDetail = ({pathId}) => {
                                 {game.name}
                             </motion.h3>
                             <p>Rating: {game.rating}</p>
+                            {getStars()}
                         </div>
                         <Info>
                             <h3>Platforms</h3>
@@ -129,6 +145,12 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between; 
+  
+  img{
+  width: 2rem;
+  height: 2rem;
+  display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
