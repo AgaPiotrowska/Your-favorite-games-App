f13fe0fadca4ea346d5cd99abf6ad4ae919dbcd8
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-14 10:55:28 +0100

    reducer and action created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-
+import React, {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {loadGames} from "./actions/gamesAction";
 
 function App() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadGames());
+    });
+
   return (
     <div className="App">
       <h1>Hi</h1>
