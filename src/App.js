df92f4895dc3659f7e3a2bff30cf428651d254f9
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-14 15:07:41 +0100

    global styles added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
+
 
 function App() {
 
   return (
     <div className="App">
+        <GlobalStyles/>
         <Home/>
     </div>
   );
