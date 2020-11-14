781c5854459c5b6af15c79d990fd3faf2a08dc0f
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-14 16:01:20 +0100

    new api request added (details of the game)

--- src/api.js
+++ src/api.js
@@ -33,3 +33,4 @@ export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
 export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
