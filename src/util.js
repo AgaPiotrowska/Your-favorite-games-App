0578915530e61b5a1b3f161bd5ce07c38898679b
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-15 15:32:11 +0100

    nav added

---
+++ src/util.js
@@ -0,0 +1,12 @@
+
+const smallImage = (imagePath, size) => {
+    const image = imagePath.match(/media\/screenshots/)
+        ? imagePath.replace(
+            "media/screenshots",
+            `media/resize/${size}/-/screenshots`
+        )
+        : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`);
+    return image;
+};
+
+export default smallImage;
